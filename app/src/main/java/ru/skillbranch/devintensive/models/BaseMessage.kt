1fd5e10c161359ae15f7479b31989d47e52c1a24
toha <toha.chugunov@mail.ru>
2021-04-22 15:27:16 +0500

    Второе задание сделано

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -0,0 +1,24 @@
+package ru.skillbranch.devintensive.models
+
+import java.util.*
+
+abstract class BaseMessage(
+    val id: String,
+    val from: User?,
+    val chat: Chat,
+    val isIncoming: Boolean = false,
+    val date: Date = Date()){
+
+    abstract fun formatMessage(): String
+
+    companion object AbstractFactory{
+        var lastId: Int = -1;
+        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String = "text", payload: Any?, isIncoming: Boolean = false): BaseMessage{
+            return when (type){
+                "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String?, isIncoming = isIncoming)
+                else ->
+                    TextMessage("$lastId", from, chat, date = date, text = payload as String?, isIncoming = isIncoming)
+            }
+        }
+    }
+}
