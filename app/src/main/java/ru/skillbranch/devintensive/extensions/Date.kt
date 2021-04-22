d4b2d88246c21250246a503861b83fd86be01662
toha <toha.chugunov@mail.ru>
2021-04-22 21:39:43 +0500

    Седьмое задание начал делать

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -31,9 +31,9 @@ fun Date.humanizeDiff(date: Date = Date()): String{
     var time = this.time
     var timeCompare = date.time
     when((timeCompare-time) * SECOND){
-        0..1 -> humanDate = "только что"
+        in 0..1 -> humanDate = "только что"
+        else -> humanDate = ""
     }
-
     return humanDate
 }
 
