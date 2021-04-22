da2fee86d9a8c273a231b037cb5da293e26fe54a
toha <toha.chugunov@mail.ru>
2021-04-22 16:31:28 +0500

    Третье задание сделано

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,7 +3,6 @@ package ru.skillbranch.devintensive.utils
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?>{
 
-        
         val parts: List<String>? = fullName?.split(" ")
         val firstName = parts?.getOrNull(0)
         val lastName = parts?.getOrNull(1)
