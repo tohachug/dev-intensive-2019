00d28791aa58bedb0c8333835929510ff9faf0a8
toha <toha.chugunov@mail.ru>
2021-04-22 17:14:28 +0500

    Пятое задание сделано

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -3,7 +3,33 @@ package ru.skillbranch.devintensive.extensions
 import java.text.SimpleDateFormat
 import java.util.*
 
+const val SECOND = 1000L
+const val MINUTE = SECOND * 60
+const val HOUR = MINUTE * 60
+const val DAY = HOUR * 24
+
 fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String{
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
 }
+
+fun Date.add(value: Int, timeUnits: TimeUnits = TimeUnits.SECOND): Date {
+    var time = this.time
+
+    time += when (timeUnits){
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
+    }
+    this.time = time
+    return this
+}
+
+enum class TimeUnits{
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY
+}
+
