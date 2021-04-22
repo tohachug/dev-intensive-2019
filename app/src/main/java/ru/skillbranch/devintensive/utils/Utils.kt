88a1bdebdfd2d7a4adf38f9834b62c8b045a6021
toha <toha.chugunov@mail.ru>
2021-04-22 21:01:35 +0500

    Шестое задание сделано

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -15,10 +15,82 @@ object Utils {
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
-    fun transliteration(divider: String? = ""): String?{
-        val cirilicToLatin: String = "Cyrillic-Latin"
-        val transliterator = Transliterator.getInstance(cirilicToLatin)
-        val result: String = transliterator.transliterate(divider)
-        return result
+    fun transliteration(divider: String = ""): String{
+        var latinString: String = ""
+//        val cirilicToLatin: String = "Cyrillic-Latin"
+//        val transliterator = Transliterator.getInstance(cirilicToLatin)
+//        val result: String = transliterator.transliterate(divider)
+        for(it in divider){
+            when(it.toString()){
+                "а" -> latinString + "a"
+                "б" -> latinString + "b"
+                "в" -> latinString + "v"
+                "г" -> latinString + "g"
+                "д" -> latinString + "d"
+                "е" -> latinString + "e"
+                "ё" -> latinString + "e"
+                "ж" -> latinString + "zh"
+                "з" -> latinString + "z"
+                "и" -> latinString + "i"
+                "й" -> latinString + "i"
+                "к" -> latinString + "k"
+                "л" -> latinString + "l"
+                "м" -> latinString + "m"
+                "н" -> latinString + "n"
+                "о" -> latinString + "o"
+                "п" -> latinString + "p"
+                "р" -> latinString + "r"
+                "с" -> latinString + "s"
+                "т" -> latinString + "t"
+                "у" -> latinString + "u"
+                "ф" -> latinString + "f"
+                "х" -> latinString + "h"
+                "ц" -> latinString + "c"
+                "ч" -> latinString + "ch"
+                "ш" -> latinString + "sh"
+                "щ" -> latinString + "sh'"
+                "ъ" -> latinString + ""
+                "ы" -> latinString + "i"
+                "ь" -> latinString + ""
+                "э" -> latinString + "e"
+                "ю" -> latinString + "yu"
+                "я" -> latinString + "ya"
+                "А" -> latinString + "A"
+                "Б" -> latinString + "B"
+                "В" -> latinString + "V"
+                "Г" -> latinString + "G"
+                "Д" -> latinString + "D"
+                "Е" -> latinString + "E"
+                "Ё" -> latinString + "E"
+                "Ж" -> latinString + "ZH"
+                "З" -> latinString + "Z"
+                "И" -> latinString + "I"
+                "Й" -> latinString + "I"
+                "К" -> latinString + "K"
+                "Л" -> latinString + "L"
+                "М" -> latinString + "M"
+                "Н" -> latinString + "N"
+                "О" -> latinString + "O"
+                "П" -> latinString + "P"
+                "Р" -> latinString + "R"
+                "С" -> latinString + "S"
+                "Т" -> latinString + "T"
+                "У" -> latinString + "U"
+                "Ф" -> latinString + "F"
+                "Х" -> latinString + "H"
+                "Ц" -> latinString + "C"
+                "Ч" -> latinString + "CH"
+                "Ш" -> latinString + "SH"
+                "Щ" -> latinString + "SH'"
+                "Ъ" -> latinString + ""
+                "Ы" -> latinString + "I"
+                "Ь" -> latinString + ""
+                "Э" -> latinString + "E"
+                "Ю" -> latinString + "YU"
+                "Я" -> latinString + "YA"
+                else -> latinString + it
+            }
+        }
+        return latinString
     }
 }
