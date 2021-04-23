067911969f45b5830627a47e964768bf01a405c4
toha <toha.chugunov@mail.ru>
2021-04-23 14:06:11 +0500

    Девятое задание сделал

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,2 +1,9 @@
 package ru.skillbranch.devintensive.extensions
 
+fun String.truncate(value: Int = 16): String{
+    return if(length > value){
+        substring(0, value).dropLastWhile{it == ' '}.plus("...")
+    } else{
+        this
+    }
+}
